f5f57c747f95fb3e492ee636bc259690650758c8
Stephen Nancekivell <stephennancekivell@gmail.com>
2013-09-30 22:30:03 +1300

    update karma

---
+++ test/appSpec.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/* jasmine specs for controllers go here */
+
+describe('app', function(){
+  beforeEach(module('app'));
+
+
+  it('should ....', inject(function() {
+  	expect('a').toEqual('b');
+    //spec body
+  }));
+
+  it('should ....', inject(function() {
+    //spec body
+  }));
+});
